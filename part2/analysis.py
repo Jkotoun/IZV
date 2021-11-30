33d3360813689dadd4e5abb07b02dc608fc0a270
Jkotoun <Jkotoun58@gmail.com>
2021-11-30 09:42:45 +0100

    1st task done

--- part2/analysis.py
+++ part2/analysis.py
@@ -25,10 +25,17 @@ Poznámka: zobrazujte na 1 desetinné místo (.1f) a počítejte, že 1 MB = 1e6
 
 def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
     df = pd.read_pickle(filename)
-    print(f"orig_size: {((df.memory_usage(deep=True).sum())/1048576):.1f} MB")
+    if verbose:
+        print(f"orig_size: {(df.memory_usage(deep=True).sum()/1048576):.1f} MB")
     
-    
-    print(f"new_size: {((df.memory_usage(deep=True).sum())/1048576):.1f} MB")
+    for col in df.columns:
+        if col == "p2a":
+            df["date"] = pd.to_datetime(df["p2a"])
+        if col in ["p2a", "h", "i", "k", "l", "n", "o", "p", "q", "t"]:
+            df[col] = pd.Categorical(df[col])
+
+    if verbose:
+        print(f"new_size: {(df.memory_usage(deep=True).sum()/1048576):.1f} MB")
     return df
 
 # Ukol 2: počty nehod v jednotlivých regionech podle druhu silnic
@@ -51,7 +58,7 @@ if __name__ == "__main__":
     # zde je ukazka pouziti, tuto cast muzete modifikovat podle libosti
     # skript nebude pri testovani pousten primo, ale budou volany konkreni ¨
     # funkce.
-    df = get_dataframe("accidents.pkl.gz") # tento soubor si stahnete sami, při testování pro hodnocení bude existovat
+    df = get_dataframe("accidents.pkl.gz", verbose=True) # tento soubor si stahnete sami, při testování pro hodnocení bude existovat
     plot_roadtype(df, fig_location="01_roadtype.png", show_figure=True)
     plot_animals(df, "02_animals.png", True)
     plot_conditions(df, "03_conditions.png", True)
