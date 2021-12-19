b5bbe305c7f38c9f837f4fe912a687b097f6169b
Jkotoun <jkotoun58@gmail.com>
2021-12-19 19:57:05 +0100

    task2 done

--- part3/geo.py
+++ part3/geo.py
@@ -11,7 +11,9 @@ import sklearn.cluster as skl
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # muzete pridat vlastni knihovny
+
 def save_and_show(fig_location: str = None, show_figure: bool = False):
+    """Ulozeni a zobrazeni obrazku podle zadanych parametru"""
     if fig_location:
         dir_path = os.path.dirname(fig_location)
         if(dir_path):
@@ -24,7 +26,7 @@ def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
     df.dropna(subset=["d", "e"], inplace=True)
     df["date"] = pd.to_datetime(df["p2a"])
-    #s-jtsk, pouzivano v cr
+    #s-jtsk format
     return  geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df["d"], df["e"]), crs="EPSG:5514")
 
 
@@ -33,22 +35,25 @@ def plot_geo(
 ):
     """Vykresleni grafu s sesti podgrafy podle lokality nehody
     (dalnice vs prvni trida) pro roky 2018-2020"""
+
     gdf = gdf.loc[gdf["region"] == "VYS", ["date", "geometry", "p36"]]
+     #convert to webmap format
     gdf.to_crs(epsg=3857, inplace=True)
+
     fig, ax = plt.subplots(3, 2, figsize=(16,27))
-
     for index, year in enumerate([2018,2019,2020]):
+        #1st class road and highway - highway = 0 and 1st class road = 1 in dataset
+        for j, road in enumerate(
+            [
+                {"name": "dálnice", "color": "green", "dataset_num":0}, 
+                {"name": "silnice 1. třídy", "color": "darkred", "dataset_num":1}
+            ]
+        ):
+            gdf[(gdf["p36"] == road["dataset_num"]) & (gdf["date"].dt.year == year)].plot(ax=ax[index, j], color=road["color"], markersize=2)
+            ax[index, j].axis("off")
+            ctx.add_basemap(ax=ax[index, j], crs=gdf.crs.to_string(), source=ctx.providers.Stamen.TonerLite)
+            ax[index, j].set_title(f"Kraj Vysočina: {road['name']}  {year}")
 
-        gdf[(gdf["p36"] == 0) & (gdf["date"].dt.year == year)].plot(ax=ax[index, 0], color="green", markersize=2)
-        gdf[(gdf["p36"] == 1) & (gdf["date"].dt.year == year)].plot(ax=ax[index, 1], color="darkred", markersize=2)
-        ctx.add_basemap(ax=ax[index, 0], crs=gdf.crs.to_string(), source=ctx.providers.Stamen.TonerLite)
-        ctx.add_basemap(ax=ax[index, 1], crs=gdf.crs.to_string(), source=ctx.providers.Stamen.TonerLite)
-
-        ax[index, 0].axis("off")
-        ax[index, 1].axis("off")
-
-        ax[index, 0].set_title(f"Kraj Vysočina: dálnice {year}")
-        ax[index, 1].set_title(f"Kraj Vysočina: silnice 1. třídy {year}")
     save_and_show(fig_location, show_figure)
     
 
@@ -57,25 +62,30 @@ def plot_cluster(
     gdf: geopandas.GeoDataFrame, fig_location: str = None, show_figure: bool = False
 ):
     """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
+    #convert to webmap format
     gdf.to_crs(epsg=3857, inplace=True)
     filtered_gdf = gdf.loc[(gdf["p36"]==1) & (gdf["region"] == "VYS")]
-    #TODO wtf
-    coords = np.dstack([filtered_gdf.geometry.x, filtered_gdf.geometry.y]).reshape(-1, 2)
-    clusters = sklearn.cluster.MiniBatchKMeans(n_clusters=100).fit(coords)
+    
+    #get coords array
+    coords_list = np.dstack([filtered_gdf.geometry.x, filtered_gdf.geometry.y])
+    coords_list = np.squeeze(coords_list, axis=0)
+    clusters = sklearn.cluster.MiniBatchKMeans(n_clusters=50).fit(coords_list)
 
-
+    #dissolve by clusters
     clustered_gdf = filtered_gdf.copy() 
     clustered_gdf["cluster"] = clusters.labels_
     clustered_gdf = clustered_gdf.dissolve(by="cluster", aggfunc={"p1": "count"})
+    
+    #plot graph
     fig, ax = plt.subplots(1,1, figsize=(15,15))
     ax.set_title("Nehody v kraji Vysočina na silnicích 1. třídy")
     ax.axis("off")
-
+    #adjust size of legend
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.1)
-    
-    clustered_gdf.plot(column="p1", ax=ax,legend=True, markersize=3, cax=cax)
+    clustered_gdf.plot(column="p1", ax=ax, legend=True, markersize=3, cax=cax)
     ctx.add_basemap(ax=ax, crs=clustered_gdf.crs.to_string() ,source=ctx.providers.Stamen.TonerLite)
+
     save_and_show(fig_location, show_figure)
 
 
