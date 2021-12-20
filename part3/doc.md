ce3bb8dca9f4d44fc46c59ae49f3d767c8822b5f
Jkotoun <Jkotoun58@gmail.com>
2021-12-20 15:23:45 +0100

    task3 wip

---
+++ part3/doc.md
@@ -0,0 +1,20 @@
+# 5 nejčastějších příčin nehod v ČR zaviněných řidičem
+Na grafu vizalizujícím nejčastější příčiny nehod můžeme vidět, že nejčastější příčinou nehody, která byla způsobena řidičem, je nevěnování se řízení. Touto příčinou bylo zaviněno 20% všech nehod, což je skoro 2x více, než 2. nejčastější příčina - nesprávné otáčení nebo couvání. To bylo příčinou v 10.4% nehodách v ČR.
+![](fig.png)
+# Nejčastější příčiny nehod, které byly zaviněny řidičem, v jednotlivých krajích
+| Kraj   | Příčina nehody                         |   Počet nehod |
+|:-------|:---------------------------------------|--------------:|
+| PHA    | Jiný druh nesprávného způsobu jízdy    |         22123 |
+| ULK    | Řidič se plně nevěnoval řízení vozidla |         15515 |
+| JHM    | Řidič se plně nevěnoval řízení vozidla |         14216 |
+| STC    | Řidič se plně nevěnoval řízení vozidla |         13781 |
+| ZLK    | Řidič se plně nevěnoval řízení vozidla |          7029 |
+| MSK    | Nepsrávné otáčení nebo couvání         |          6431 |
+| JHC    | Řidič se plně nevěnoval řízení vozidla |          5215 |
+| PAK    | Řidič se plně nevěnoval řízení vozidla |          4914 |
+| KVK    | Řidič se plně nevěnoval řízení vozidla |          3824 |
+| OLK    | Nepsrávné otáčení nebo couvání         |          3285 |
+| HKK    | Řidič se plně nevěnoval řízení vozidla |          3280 |
+| PLK    | Nepsrávné otáčení nebo couvání         |          3134 |
+| LBK    | Nezvládnutí řízení vozidla             |          2993 |
+| VYS    | Řidič se plně nevěnoval řízení vozidla |          2741 |
--- part3/doc.py
+++ part3/doc.py
@@ -10,6 +10,9 @@ def plot_graph(df: pd.DataFrame):
     df = df.loc[df["p10"]==1]
     df1 = df.groupby(["p12"]).size().reset_index(name="pocet").sort_values(by=['pocet'], ascending=False)[:5]
     
+    stats(df, df1)
+
+
     df1["labels"] = ["Nevěnování se řízení", "Nesprávné otáčení nebo couvání",  "Jiný druh nesprávného způsobu jízdy", "Nedodržení bezpečné vzdálenosti","Nepřízpůsobení rychlosti vlastnostem vozidla"]
     g = sns.catplot(data=df1, x="labels", y="pocet", kind="bar", aspect=3 , margin_titles=True)
     g.fig.suptitle("5 nejčastějčích příčin u nehod zaviněných řidičem")
@@ -19,6 +22,29 @@ def plot_graph(df: pd.DataFrame):
 
 
 
+
+def table(df: pd.DataFrame):
+    df = df.loc[df["p10"]==1]
+    df2 = df.groupby(["region","p12"]).size().reset_index(name="count").sort_values(by="count", ascending=False).groupby(["region"]).head(1)
+    causes = {
+        516: "Jiný druh nesprávného způsobu jízdy",
+        508: "Řidič se plně nevěnoval řízení vozidla",
+        504: "Nepsrávné otáčení nebo couvání",
+        511: "Nezvládnutí řízení vozidla"
+    }
+    df2["p12"] = df2["p12"].map(causes)
+    
+    # df2["procento"] = df2["count"]/len(df.loc[df["region"]==df2["region"]])
+    # df2["procento_nehod"] = (df2["pocet"]/len(df))*100
+    df2 = df2.rename(columns={"region":"Kraj", "p12": "Příčina nehody", "count": "Počet nehod"})
+    print(df2.to_markdown(index=False))
+
+def stats(df:pd.DataFrame, dftop: pd.DataFrame):
+    dftop["procent_nehod"] = (dftop["pocet"]/len(df))*100
+    print(dftop)
+
+
 if __name__ == "__main__":
     df = pd.read_pickle("accidents.pkl.gz")
     plot_graph(df)
+    # table(df)
